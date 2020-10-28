a65927715c9ed098cf6d32da0fc3f796c5c59294
ishizakiharuhiko <73152253+ishizakiharuhiko@users.noreply.github.com>
2020-10-28 16:38:14 +0900

    修正done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :installs
+  devise_for :users
   get 'messages/index'
   root to: "messages#index"
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
